b630821a995486f4d4eb86f676d893a2e03f2389
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-28 23:50:06 +0530

    ADD:Player choose x or o

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,21 +1,60 @@
-public class GameBoard {
-	
-	public static char[] createboard()
+import java.util.Scanner;
+
+public class UserChoose {
+
+	static char playerSymbol, computerSymbol;
+	static Scanner sc = new Scanner(System.in);
+
+	//UC-1 Creating an empty Board 
+	public static char[] createEmptyBoard() 
 	{
-		char [] board = new char[10];
-				for(int i=1;i<10;i++)
-				{
-					board[i] = ' ';
-				}
-				
-				return board;
-    
-    }
-	
-	public static void main(String args[])
-	{
-		char[] board = createboard();
+		char[] board = new char[10];
+		for (int i = 1; i < board.length; i++) 
+		{
+			board[i] = ' ';
+		}
+		return board;
 	}
+
+	 
+	public static char chooseXorO() {
+		System.out.println("Enter the symbol:'X' or 'O' ");
+		char symbol = Character.toUpperCase(sc.next().charAt(0));
+		if (symbol == 'X' || symbol == 'O') {
+			playerSymbol(symbol);
+			return symbol;
+		} else {
+			System.out.println("Invalid Character. Try Again.");
+			return chooseXorO();
+		}
+	}
+
+	 
+	public static void playerSymbol(char playerSymbols) {
+
+		if(playerSymbols == 'X')
+		{
+			playerSymbol = 'X';
+			computerSymbol = 'O';
+		}
+		else if(playerSymbols == 'O')
+		{
+			playerSymbol = 'O';
+			computerSymbol = 'X';
+		}
+		System.out.println("player letter to play is "+playerSymbol);
+		System.out.println("computer letter to play is "+computerSymbol);
+
+	}
+
+
+	public static void main(String[] args) {
+
+		char[] board = createEmptyBoard();
+		playerSymbol = chooseXorO();
+
+	}
+
 }
 
 
