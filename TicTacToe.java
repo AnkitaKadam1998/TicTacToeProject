b38b17f71612ef1ef1348b337fd2de7334bdc362
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-29 00:49:17 +0530

    ADD:Final Output

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class ShowBoard {
+public class FinalOutput {
 
-	static char playerSymbol, computerSymbol;
+	static final int HEAD = 1, TAIL = 0;
+	static String lastPlayed; 
+	static char playerSymbol;
+	static char computerSymbol;
 	static Scanner sc = new Scanner(System.in);
 
-	 
+	//  Creating an empty Board 
 	public static char[] createEmptyBoard() 
 	{
-		char[] board = new char[10];
+		char[] board = new char[9];
 		for (int i = 1; i < board.length; i++) 
 		{
 			board[i] = ' ';
@@ -16,7 +19,7 @@ public class ShowBoard {
 		return board;
 	}
 
-	 
+	// Taking Input from player to choose between X or O 
 	public static char chooseXorO() {
 		System.out.println("Enter the symbol:'X' or 'O' ");
 		char symbol = Character.toUpperCase(sc.next().charAt(0));
@@ -29,7 +32,7 @@ public class ShowBoard {
 		}
 	}
 
-	 
+	// Determine letter for player and computer to play game 
 	public static void playerSymbol(char playerSymbols) {
 
 		if(playerSymbols == 'X')
@@ -47,22 +50,19 @@ public class ShowBoard {
 
 	}
 
-
-	
-	public static void displayBoard(char[] board) {
-		System.out.println("\n\t " + board[1] + " | " + board[2] + " | " + board[3] + "\n\t-----------");
-		System.out.println("\t " + board[4] + " | " + board[5] + " | " + board[6] + "\n\t-----------");
-		System.out.println("\t " + board[7] + " | " + board[8] + " | " + board[9] + "\n\t");
+	//showing empty Board 
+	public static void showBoard(char[] board)
+	{
+		System.out.println("--------------");
+		System.out.println("|  " +board[0]+ " | "+board[1] + " | "+board[2]+ " | ");
+		System.out.println("--------------");
+		System.out.println("|  " +board[3]+ " | "+board[4] + " | "+board[5]+ " | ");
+		System.out.println("--------------");
+		System.out.println("|  " +board[6]+ " | "+board[7] + " | "+board[8]+ " | ");
+		System.out.println("--------------");
 	}
 
-	private static boolean isEmpty(char[] board) {
-		for(char cell: board) {
-			if(cell == ' ')
-				return true;
-		}
-		return false;
-	}
-
+	//to choose desired location in a board
 	public static void chooseLocatin(char [] board)
 	{	
 		System.out.println("check the index you want to play");
@@ -134,18 +134,17 @@ public class ShowBoard {
 		}
 	}
 
-	
 	//to take player move
 	public static void movePlayer(char[] board) {
 		System.out.print("Enter the index you want to move to: ");
 		int index = sc.nextInt();
 		while (index < 0 || index > 8) {
-			System.out.print("Wrong Input. Try Again : ");
+			System.out.print("invalid Input. Try Again : ");
 			index = sc.nextInt();
 		}
 		if (board[index] == ' ') {
 			board[index] = playerSymbol;
-			displayBoard(board);
+			showBoard(board);
 		} else {
 			System.out.println("Index not available. Choose another");
 			movePlayer(board);
@@ -153,20 +152,27 @@ public class ShowBoard {
 	}
 
 	// toss to who plays first
-		public static void toss(char[] board) {
-			int toss = (int) (Math.random() * 2 % 2);
-			if (toss == HEAD) {
-				System.out.println("Player Wins the Toss.");
-				lastPlayed = "Player";
-				movePlayer(board);						
-			}else {
-				System.out.println("Computer Wins the Toss.");
-				lastPlayed = "Computer";
+	public static void toss(char[] board) {
+		int toss = (int) (Math.random() * 2 % 2);
+		if (toss == HEAD) {
+			System.out.println("Player Wins the Toss.");
+			lastPlayed = "Player";
+			movePlayer(board);						
+		}else {
+			System.out.println("Computer Wins the Toss.");
+			lastPlayed = "Computer";
 
-			}
 		}
+	}
 
-		public static void moveComputer(char[] board) {
+	/* UC8 -- UC12 Computer Movement
+	 * 8.  check if computer can win
+	 * 9.  check if player can win
+	 * 10. check if diagonal cells are available
+	 * 11. check if center is available. If not, take any sides.
+	 * 12. check if board is full or one of the players wins
+	 */
+	public static void moveComputer(char[] board) {
 		int checkCompWinPos = checkIsWinning(board, computerSymbol); 
 		int checkPlayWinPos = checkIsWinning(board, playerSymbol);
 
@@ -178,46 +184,63 @@ public class ShowBoard {
 				startGame();
 			else
 				System.exit(0);			
+		}																					//UC8
+		else if( checkPlayWinPos != 0 )														//UC9
+			board[checkPlayWinPos] = computerSymbol;		
+		else {		
+			if(board[1] == ' ')																//UC10
+				board[1] = computerSymbol;
+			else if(board[3] == ' ')
+				board[3] = computerSymbol;
+			else if(board[7] == ' ')
+				board[7] = computerSymbol;
+			else if(board[9] == ' ')
+				board[9] = computerSymbol;
+			else if(board[5] == ' ')
+				board[5] = computerSymbol;													//UC11
+			else if(board[2] == ' ')
+				board[2] = computerSymbol;
+			else if(board[4] == ' ')
+				board[4] = computerSymbol;
+			else if(board[6] == ' ')
+				board[6] = computerSymbol;
+			else if(board[8] == ' ')
+				board[8] = computerSymbol;
 		}		
-		
+		showBoard(board);
+		lastPlayed = "Computer";
+		if(checkWin(board)) {																//UC12 -- Computer Wins
+			System.out.println("Computer Won The Game !! \nDo You Want to Play Another Game (Y/N) : ");
+			if(Character.toUpperCase(sc.next().charAt(0)) == 'Y')							//UC13 -- Next Game
+				startGame();
+			else
+				System.exit(0);
+		}
+		if(isEmpty(board)) {
+			movePlayer(board);
+		}else {
+			System.out.println("Game Tied.");												//UC12 -- board is full
+			System.exit(0);
+		}
 	}
-		
-		private static int checkIsWinning(char[] board, char letter) {
-			int index = 1;
-			while(index > 0 && index < 10) {
-				if(board[index] == ' ') {
-					board[index] = letter;
-					if(checkWin(board)) {
-						return index;
-					}
-					else {
-						board[index] = ' ';
-					}
+
+
+	private static int checkIsWinning(char[] board, char letter) {
+		int index = 1;
+		while(index > 0 && index < 10) {
+			if(board[index] == ' ') {
+				board[index] = letter;
+				if(checkWin(board)) {
+					return index;
 				}
-				index++;
+				else {
+					board[index] = ' ';
+				}
 			}
-			return 0;
+			index++;
 		}
-
-		private static boolean isEmpty(char[] board) {
-			for(char cell: board) {
-				if(cell == ' ')
-					return true;
-			}
-			return false;
-		}
-
-
-		private static boolean checkWin(char[] board) {
-			return ((board[1] == board[2] && board[2] == board[3] && board[1] != ' ') 						//top-row	
-					|| (board[4] == board[5] && board[5] == board[6] && board[4] != ' ')					//middle-row
-					|| (board[7] == board[8] && board[8] == board[9] && board[7] != ' ') 					//bottom-row					
-					|| (board[1] == board[4] && board[4] == board[7] && board[1] != ' ') 					//left-column
-					|| (board[2] == board[5] && board[5] == board[8] && board[2] != ' ')					//middle-column
-					|| (board[3] == board[6] && board[6] == board[9] && board[3] != ' ') 					//right-column
-					|| (board[1] == board[5] && board[5] == board[9] && board[1] != ' ') 					//left-diagonal
-					|| (board[3] == board[5] && board[5] == board[7] && board[3] != ' '));					//right-diagonal
-		}
+		return 0;
+	}
 
 		private static boolean areMovesLeft(char[] board) {
 			for (int pos = 1; pos < board.length; pos++) {
